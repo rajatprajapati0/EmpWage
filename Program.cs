a8db6e2b7a5ff558f23435f6f24969e862f12c7f
Rajat <rajatprajapati09@gmail.com>
2023-05-17 16:47:43 +0530

    checking employee attendenc

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        const int attendence = 1;
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int check=random.Next(0,2);
+            if (check ==attendence)
+            {
+                Console.WriteLine("present");
+            }
+            else
+            {
+                Console.WriteLine("absent");
+            }
+        }
+    }
+}
