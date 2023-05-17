4b497dd7916bece0877c9ffea45af18c1576503c
Rajat <rajatprajapati09@gmail.com>
2023-05-17 16:59:31 +0530

    claculating also half day wage

--- Program.cs
+++ Program.cs
@@ -4,17 +4,24 @@ namespace EmployeeWage
 {
     internal class Program
     {
-        const int attendence = 1;
+        const int fullDay = 1;
+        const int halfDay = 2;
         const int perHourWage = 20;
         const int fullDayHaour = 8;
+        const int halfDahour = 4;
         static void Main(string[] args)
         {
             Random random = new Random();
            int empHours = 0;
-            int check=random.Next(0,2);
-            if (check ==attendence)
+            int check=random.Next(0,3);
+            if (check ==fullDay)
             {
                 empHours = fullDayHaour;
+            }
+            else if (check == halfDay) 
+            {
+                empHours = halfDahour;
+             
             }
             else
             {
