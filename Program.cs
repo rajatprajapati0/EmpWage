bd7138e6ce6dd11f0538b8c7bed4f6bab7e9b2e8
Rajat <rajatprajapati09@gmail.com>
2023-05-17 16:55:00 +0530

    calculating daily Wage

--- Program.cs
+++ Program.cs
@@ -5,18 +5,27 @@ namespace EmployeeWage
     internal class Program
     {
         const int attendence = 1;
+        const int perHourWage = 20;
+        const int fullDayHaour = 8;
         static void Main(string[] args)
         {
             Random random = new Random();
+           int empHours = 0;
             int check=random.Next(0,2);
             if (check ==attendence)
             {
-                Console.WriteLine("present");
+                empHours = fullDayHaour;
             }
             else
             {
-                Console.WriteLine("absent");
+                empHours = 0;
             }
+
+            int dailyWage = empHours * perHourWage;
+
+            Console.WriteLine(dailyWage);
         }
+
     }
 }
+    
